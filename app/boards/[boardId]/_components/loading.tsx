c4865ea67b5946b1e43c352cc44a77ff40d329a3
Ismaël MOHAMED BOUH <ismaelmedbouh@outlook.fr>
2024-03-27 18:38:07 +0100

    Room Info

--- app/boards/[boardId]/_components/loading.tsx
+++ app/boards/[boardId]/_components/loading.tsx
@@ -1,16 +1,16 @@
 import { Loader } from 'lucide-react';
 
-import Info from '@/app/boards/[boardId]/_components/info';
-import Participants from '@/app/boards/[boardId]/_components/participants';
-import Toolbar from '@/app/boards/[boardId]/_components/toolbar';
+import { InfoSkeleton } from '@/app/boards/[boardId]/_components/info';
+import { ParticipantsSkeleton } from '@/app/boards/[boardId]/_components/participants';
+import { ToolbarSkeleton } from '@/app/boards/[boardId]/_components/toolbar';
 
 const Loading = () => {
   return (
     <main className="h-full w-full relative bg-neutral-100 touch-none flex items-center justify-center">
       <Loader className="h-6 w-6 text-muted-foreground animate-spin" />
-      <Info.Skeleton />
-      <Participants.Skeleton />
-      <Toolbar.Skeleton />
+      <InfoSkeleton />
+      <ParticipantsSkeleton />
+      <ToolbarSkeleton />
     </main>
   );
 };
