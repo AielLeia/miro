fe227d3b2872100366c0dd883ef3bc114c6be175
Ismaël MOHAMED BOUH <ismaelmedbouh@outlook.fr>
2024-03-31 17:14:55 +0200

    Layer Depth

--- app/boards/[boardId]/_components/selection-tools.tsx
+++ app/boards/[boardId]/_components/selection-tools.tsx
@@ -4,7 +4,7 @@ import { useDeleteLayers } from '@/hooks/use-delete-layers';
 import { useSelectionBounds } from '@/hooks/use-selection-bounds';
 import { useMutation, useSelf } from '@/liveblocks.config';
 import { Camera, Color } from '@/types/canvas';
-import { Trash2 } from 'lucide-react';
+import { BringToFront, SendToBack, Trash2 } from 'lucide-react';
 import { Dispatch, memo, SetStateAction } from 'react';
 
 import ColorPicker from '@/app/boards/[boardId]/_components/color-picker';
@@ -22,6 +22,49 @@ const SelectionTools = memo(
     const selectionBounds = useSelectionBounds();
 
     const deleteLayer = useDeleteLayers();
+
+    const moveToBack = useMutation(
+      ({ storage }) => {
+        const liveLayerIds = storage.get('layerIds');
+        const indices: number[] = [];
+
+        const arr = liveLayerIds.toArray();
+
+        for (let i = 0; i < arr.length; i++) {
+          if (selection.includes(arr[i])) {
+            indices.push(i);
+          }
+        }
+
+        for (let i = 0; i < indices.length; i++) {
+          liveLayerIds.move(indices[i], i);
+        }
+      },
+      [selection]
+    );
+
+    const bringToFront = useMutation(
+      ({ storage }) => {
+        const liveLayerIds = storage.get('layerIds');
+        const indices: number[] = [];
+
+        const arr = liveLayerIds.toArray();
+
+        for (let i = 0; i < arr.length; i++) {
+          if (selection.includes(arr[i])) {
+            indices.push(i);
+          }
+        }
+
+        for (let i = indices.length - 1; i >= 0; i--) {
+          liveLayerIds.move(
+            indices[i],
+            arr.length - 1 - (indices.length - 1 - i)
+          );
+        }
+      },
+      [selection]
+    );
 
     const setFill = useMutation(
       ({ storage }, fill: Color) => {
@@ -45,6 +88,18 @@ const SelectionTools = memo(
         }}
       >
         <ColorPicker onChange={setFill} />
+        <div className="flex flex-col gap-y-0.5">
+          <Hint label={'Bring to Front'}>
+            <Button onClick={bringToFront} variant="board" size="icon">
+              <BringToFront />
+            </Button>
+          </Hint>
+          <Hint label={'Send to back'} side={'bottom'}>
+            <Button onClick={moveToBack} variant="board" size="icon">
+              <SendToBack />
+            </Button>
+          </Hint>
+        </div>
         <div className="flex items-center pl-2 ml-2 border-l border-neutral-200">
           <Hint label={'Delete'}>
             <Button variant="board" size="icon" onClick={deleteLayer}>
